c4332a936cc6f90533b499e50e28f49d7aa96717
leoducapa <159541703+leoducapa@users.noreply.github.com>
2024-11-14 14:11:40 -0300

    ALELUIA

--- notificação.js
+++ notificação.js
@@ -1,10 +1,10 @@
 function mostrarNotificacao() {
     const notificacao = document.getElementById("notificacao");
 
-    // Adiciona a classe para mostrar a notificação
+    
     notificacao.classList.add("mostrar");
 
-    // Remove a notificação após 3 segundos
+    
     setTimeout(() => {
         notificacao.classList.remove("mostrar");
     }, 3000);
